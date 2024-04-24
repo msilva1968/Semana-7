7044c0f44565c8f57250c3551d00fd4c73dbaa4c
Marcio Silva <marcio.silva.ter@estacio.br>
2024-04-24 15:36:31 -0300

    Semana 7 JWT

---
+++ src/modulos/usuario/usuario.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity({ name: 'usuarios' })
+export class UsuarioEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false })
+  nome: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
+  email: string;
+
+  @Exclude()
+  @Column({ name: 'senha', length: 255, nullable: false })
+  senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+
+}
